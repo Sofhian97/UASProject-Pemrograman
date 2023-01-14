c3bf423783e0826a54d71831f61d0fb0e82972ab
[Sofhian97] <[sofhianp@gmail.com]>
2023-01-14 15:33:04 +0700

    input nilai program

---
+++ input_nilai.py
@@ -0,0 +1,56 @@
+def ubah():
+    rubah = input('Ubah Biodata/Nilai [B/N] : ')
+    if rubah == 'B' or rubah == 'b':
+        i = int(input('Inputkan ID : '))
+        if (i > len(d_nim[i])):
+            print('ID Salah')
+        else:
+            jurusanb = input('Prodi [TI/TM/TL] : ')
+            if jurusanb == 'TI' or jurusanb == 'ti':
+                jbaru = 'Teknik Informatika'
+                d_jurusan[i] = jbaru
+            elif jurusanb == 'Teknik Mesin' or jurusanb == 'tm':
+                jbaru = 'Teknik Mesin'
+                d_jurusan[i] = jbaru
+            elif jurusanb == 'Teknik Listrik' or jurusanb == 'tl':
+                jbaru = 'Teknik Listrik'
+                d_jurusan[i] = jbaru
+            else:
+                kembali = input('Pilihan tidak ada')
+                ubah()
+
+            namabaru = input('Nama : ')
+            d_nama[i] = namabaru
+
+            nimbaru = input('Nim : ')
+            d_nim[i] = nimbaru
+
+            kelasbaru = input('Kelas : ')
+            d_kelas[i] = kelasbaru
+
+
+    else:
+        i = int(input('Inputkan ID : '))
+        if (i > len(d_nim[i])):
+            print('ID Salah')
+        else:
+            hadirb = float(input('Jumlah Hadir : '))
+            j_hadirb = ((hadirb / 16) * 20 / 100) * 100
+            d_hadir[i] = j_hadirb
+
+            tugasb = float(input('Nilai Tugas : '))
+            j_tugasb = tugasb * (25 / 100)
+            d_tugas[i] = j_tugasb
+
+            utsb = float(input('Nilai UTS : '))
+            j_utsb = utsb * (25 / 100)
+            d_uts[i] = j_utsb
+
+            uasb = float(input('Nilai UAS : '))
+            j_uasb = uasb * (30 / 100)
+            d_uas[i] = j_uasb
+
+            totalb = j_hadirb + j_tugasb + j_utsb + j_uasb
+            d_akhir[i] = totalb
+    kembali = input('Kembali Tekan [enter]')
+    menu_dosen()
